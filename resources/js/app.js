595f2a35116872273c15769db2d4814a1b10e15f
Darwin Llacuna <llacuna.darwin24@gmail.com>
2022-04-03 19:55:38 -0700

    add spatie laravel

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,7 +8,7 @@ const appName = window.document.getElementsByTagName('title')[0]?.innerText || '
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => require(`./Pages/${name}.vue`),
+    resolve: async (name) => (await import(`./Pages/${name}.vue`)).default,
     setup({ el, app, props, plugin }) {
         return createApp({ render: () => h(app, props) })
             .use(plugin)
